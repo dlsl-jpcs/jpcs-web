72bb31cf955d68dbe01fcac1aaeea2f934ce2c81
Tyron <lucena.tyronscott@gmail.com>
2024-08-10 14:18:16 +0800

    batman ! :D

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <div className="title-container">
+        <div className="row">
+          <div className="overlap top-part">
+            <h1 className='background-text'> JPCS </h1>
+            <h1 className='foreground-text'> JPCS </h1>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="box"></div>
+          <div className="overlap bottom-part">
+            <h1 className='background-text'> DLSL </h1>
+            <h1 className='foreground-text-2'> DLSL </h1>
+          </div>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
